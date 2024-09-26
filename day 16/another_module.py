1046e54a564cf937a37eb96ba5393ecf8bf80849
Ohwane <nwanerio@gmail.com>
2024-09-26 14:50:28 +0100

    cpe593 group

---
+++ day 16/another_module.py
@@ -0,0 +1,1 @@
+another_variable= [1,2,3,4]
---
+++ day 16/oop.py
@@ -0,0 +1,14 @@
+# from turtle import Turtle, Screen
+# import another_module
+
+# print(another_module.another_variable)
+
+# timmy=Turtle()
+# timmy.shape("turtle")
+# timmy.color("green")
+# move= timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+# print(timmy)
+ 
